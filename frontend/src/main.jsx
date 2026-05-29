8a22c3a04b8bd2fdfa0fcbb3da17f8a029cd9b38
jarkkoojala1-collab <jarkko.ojala1@gmail.com>
2026-05-29 23:01:44 +0300

    Import wind gust text cleanup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "leaflet/dist/leaflet.css";
 import "./style.css";
 import "./rain-slider.css";
+import "./wind-text-cleanup.js";
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
