aa469c2dc1a8b62e18403cca14ff45f5dd73e4d7
jarkkoojala1-collab <jarkko.ojala1@gmail.com>
2026-06-10 15:45:19 +0300

    Disable broken mobile fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,6 @@ import "./forecast-panel-cleanup.css";
 import "./area-summary-forecast.css";
 import "./area-summary-forecast.js";
 import "./km-tool.js";
-import "./km-mobile-fix.js";
 import "./wind-text-cleanup.js";
 
 class ErrorBoundary extends React.Component {
